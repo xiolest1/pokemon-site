efc5f7ae658277b08657d6758a0eb69d79143315
Johan <35781301+xiolest1@users.noreply.github.com>
2025-03-22 05:17:24 -0400

    Update setup.js

--- javascript/setup.js
+++ javascript/setup.js
@@ -1,4 +1,5 @@
-let pokemons = [' '];
+let pokemons = [' ']; // So index 1 lines up with ID=1 (Bulbasaur)
+let pokemonsLoaded = false;
 
 /**fetch pokemon name and id */
 async function getAllNames() {
@@ -12,31 +13,33 @@ async function getAllNames() {
             name: responseAsJson.results[i].name,
             types: []
         });
-    };
+    }
 
     getAllTypes();
-};
+}
 
 /**fetch pokemon types */
 async function getAllTypes() {
     for (let i = 0; i < 18; i++) {
-        let url = 'https://pokeapi.co/api/v2/type/' + (i + 1)
-        let response = await fetch(url)
-        let responseAsJson = await response.json()
+        let url = 'https://pokeapi.co/api/v2/type/' + (i + 1);
+        let response = await fetch(url);
+        let responseAsJson = await response.json();
 
-        const pokemonInType = responseAsJson.pokemon
+        const pokemonInType = responseAsJson.pokemon;
         
-        for(j = 0; j < pokemonInType.length; j++) {
-            const pokemonId = pokemonInType[j].pokemon.url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', '');
+        for(let j = 0; j < pokemonInType.length; j++) {
+            const pokemonId = pokemonInType[j].pokemon.url
+                .replace('https://pokeapi.co/api/v2/pokemon/', '')
+                .replace('/', '');
 
             if(pokemonId <= pokemons.length && pokemons[pokemonId]) {
                 pokemons[pokemonId].types.push(responseAsJson.name);
-            };
-        };
-    };
+            }
+        }
+    }
 
     loadingCompletion();
-};
+}
 
 /**hide loading div after completion */
 function loadingCompletion() {
@@ -48,8 +51,9 @@ function loadingCompletion() {
         document.body.style.overflow = 'unset';
     }, 500);
 
-    pokemons.splice(0, 1);
-    currentList = pokemons;
+    pokemons.splice(0, 1); // remove dummy at index 0
+    currentList = pokemons; // from pokemon-list.js
+    pokemonsLoaded = true;
 
     updatePokemonList();
-};
+}
